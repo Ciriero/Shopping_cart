cef909796e4c58d2973b034de97616460106aa76
Christian Calle C <chcallec@gmail.com>
2023-02-07 07:48:58 +0100

    added data books

---
+++ src/data/books.js
@@ -0,0 +1,30 @@
+import perdicion from "../images/perdicion.jpg";
+import ocaso from "../images/ocaso.jpg";
+import carruaje from "../images/carruaje.jpg";
+
+export default [
+  {
+    id: 1,
+    title: "Perdición",
+    author: "Jack Ketchum",
+    price: 19.95,
+    img: perdicion,
+    amount: 1,
+  },
+  {
+    id: 2,
+    title: "Cuentos del ocaso",
+    author: "Rhoda Broughton",
+    price: 22.95,
+    img: ocaso,
+    amount: 1,
+  },
+  {
+    id: 3,
+    title: "El carruaje fantasma y otras historias sobrenaturales",
+    author: "Amelia B. Edwards",
+    price: 20.95,
+    img: carruaje,
+    amount: 1,
+  },
+];
