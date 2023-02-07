82183c5b263e0cea40b4fad5592a4b8109ea4aab
Christian Calle C <chcallec@gmail.com>
2023-02-07 07:54:59 +0100

    added cart.js and cartitem.js

---
+++ src/components/Cart.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import books from '../data/books';
+import CartItem from './CartItem';
+
+const Cart = () => {
+  return (
+    <div>Cart</div>
+  )
+}
+
+export default Cart
