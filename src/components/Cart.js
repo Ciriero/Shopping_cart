23a28157072a47a176e9354e3753a3743f91e5e7
Christian Calle C <chcallec@gmail.com>
2023-02-07 08:17:06 +0100

    added cart.js and first styles

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,11 +1,29 @@
-import React from 'react'
-import books from '../data/books';
-import CartItem from './CartItem';
+import React from "react";
+import books from "../data/books";
+import CartItem from "./CartItem";
+import { CartContainer, ButtonRmv } from "../styles/Cart.styles";
 
 const Cart = () => {
   return (
-    <div>Cart</div>
-  )
-}
+    <CartContainer>
+      <h2>YOUR BOOKS</h2>
+      <div>
+        {books.map((item) => (
+          <CartItem key={item} {...item} />
+        ))}
+      </div>
+      <div>
+        <hr />
+        <div className="total">
+          <h3>
+            Total
+            <span>$2</span>
+          </h3>
+        </div>
+        <ButtonRmv>remove</ButtonRmv>
+      </div>
+    </CartContainer>
+  );
+};
 
-export default Cart
+export default Cart;
--- src/styles/Cart.styles.js
+++ src/styles/Cart.styles.js
@@ -1,1 +1,39 @@
 import styled from "styled-components";
+
+export const CartContainer = styled.section`
+  width: 400px;
+  height: 70vh;
+  margin: 3rem auto;
+  border: 1px solid black;
+  .total {
+    margin: 1rem;
+    h3 {
+      display: flex;
+      justify-content: space-between;
+    }
+  }
+  @media screen and (min-width: 800px) {
+    width: 80vh;
+    max-height: 70vh;
+    max-width: 80vw;
+    margin: 3rem auto;
+    border: 1px solid black;
+  }
+`;
+
+export const ButtonRmv = styled.button`
+  margin-bottom: 3rem;
+  border: none;
+  padding: 0.8rem;
+  border-radius: 0.25rem;
+  background-color: var(--clr-red2);
+  color: black;
+  transition: 0.3s all ease-in-out;
+  cursor: pointer;
+  :hover {
+    background-color: var(--clr-red);
+  }
+  @media screen and (min-width: 800px) {
+    margin-bottom: 10rem;
+  }
+`;
